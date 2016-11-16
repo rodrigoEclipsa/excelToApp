164f037ac955e5cf990e6f365159e2a46bf1fb30
rodrigo walter <rodrigo.eclipsa@gmail.com>
2016-11-16 20:10:02 -0300

    ajustes

--- src/main/java/controller/CalculateController.java
+++ src/main/java/controller/CalculateController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import static spark.Spark.after;
-import static spark.Spark.before;
 import static spark.Spark.post;
 
 import java.util.ArrayList;
@@ -32,28 +30,7 @@ public class CalculateController
 		
 		
 
-		before((request, response) -> 
-		{
-			
-	   	  response.header("Access-Control-Allow-Origin", "*");
-	      response.header("Access-Control-Request-Method","POST");
-	      response.header("Access-Control-Allow-Headers","*" );
-			
-		   // boolean authenticated = true;
-		     
-		   // if (!authenticated) {
-		     //   halt(401, "You are not welcome here");
-			
-		    //}
-			
-			
-		});
 
-
-		after((request, response) -> {
-			// response.header("Content-Encoding", "gzip");
-		});
-			
 		
 		
 	
@@ -90,6 +67,8 @@ public class CalculateController
     	 });
 		 */
 		 
+		
+	
     	 /**
     	  * 
     	  * peticion para calcular
--- src/main/java/main/ExcelToAppMain.java
+++ src/main/java/main/ExcelToAppMain.java
@@ -1,5 +1,8 @@
 package main;
 
+import static spark.Spark.after;
+import static spark.Spark.before;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -61,13 +64,38 @@ public class ExcelToAppMain {
 		
     ///------------------------------------------------------------
     	
-       new CalculateController();
+        
+		before((request, response) -> 
+		{
+			
+	   	  response.header("Access-Control-Allow-Origin", "*");
+	      response.header("Access-Control-Request-Method","POST");
+	      response.header("Access-Control-Allow-Headers","*" );
+			
+		   // boolean authenticated = true;
+		     
+		   // if (!authenticated) {
+		     //   halt(401, "You are not welcome here");
+			
+		    //}
+			
+			
+		});
+		
+
+		after((request, response) -> {
+			// response.header("Content-Encoding", "gzip");
+		});
+			
+
+		
+	    new CalculateController();
     	
-       System.out.println("server init...");
-    
+	       System.out.println("server init...");
+	    
     
 }
     
-    
+	
     
 }
